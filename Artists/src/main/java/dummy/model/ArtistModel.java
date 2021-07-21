589a448dac4addaa0d3d4413d5611d29ca081001
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2021-07-21 20:37:04 +0530

    Update ArtistModel.java
    na

--- Artists/src/main/java/dummy/model/ArtistModel.java
+++ Artists/src/main/java/dummy/model/ArtistModel.java
@@ -1,5 +1,9 @@
 package dummy.model;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+
 import org.springframework.stereotype.Component;
 
 import com.google.gson.annotations.SerializedName;
@@ -17,15 +21,21 @@ public class ArtistModel {
 	@SerializedName("ArtistId")
 	private Long ArtistId;
 	
+	@NotNull(message = "Name is required")
 	@SerializedName("name")
 	private String name;
 	
+	@NotNull(message = "Age is required")
+	@Min(0)
+	@Max(100)
 	@SerializedName("age")
 	private int age;
 	
+	@NotNull(message = "catchphrase is required")
 	@SerializedName("catchphrase")
 	private String catchphrase;
 	
+	@NotNull(message = "description is required")
 	@SerializedName("description")
 	private String description;
 
