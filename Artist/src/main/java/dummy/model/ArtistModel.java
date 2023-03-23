0d6784f88b7e54a076f9b8564bd8c9d451b05257
Sayantan Chatterjee <35490107+sayantanc6@users.noreply.github.com>
2023-03-23 09:45:15 +0530

    Update ArtistModel.java

--- Artist/src/main/java/dummy/model/ArtistModel.java
+++ Artist/src/main/java/dummy/model/ArtistModel.java
@@ -3,6 +3,7 @@ package dummy.model;
 import javax.validation.constraints.Max;
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
 
 import org.springframework.stereotype.Component;
 
@@ -38,5 +39,8 @@ public class ArtistModel {
 	@NotNull(message = "description is required")
 	@SerializedName("description")
 	private String description;
+       
+        @SerializedName("date")
+	private LocalDateTime date;
 
 }
